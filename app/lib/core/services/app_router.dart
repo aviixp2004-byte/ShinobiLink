9107703df4d2654aec6334e2d7d65e87e9c25bc4
Avinash <aviixp2004@gmail.com>
2026-07-25 09:45:02 +0530

    feat: create app router with go_router configuration

---
+++ app/lib/core/services/app_router.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../features/splash/splash_screen.dart';
+
+final GoRouter appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const SplashScreen();
+      },
+    ),
+  ],
+);
